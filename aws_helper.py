2f6542b1cfb132b873609d03bc996973a6337eb3
Alexander Ramin <alex.ramin@thoughtworks.com>
2021-08-27 12:14:07 +0200

    Update config and credentials

--- aws_helper.py
+++ aws_helper.py
@@ -3,15 +3,17 @@ import os
 
 config = configparser.ConfigParser()
 config.read_file(open('dwh.cfg'))
-KEY='KEY'
-SECRET='SECRET'
 
 CLUSTER_DB_HOST=config.get('CLUSTER','DB_HOST')
 CLUSTER_DB=config.get('CLUSTER','DB_NAME')
 CLUSTER_DB_USER=config.get('CLUSTER','DB_USER')
 CLUSTER_DB_PASSWORD=config.get('CLUSTER','DB_PASSWORD')
 CLUSTER_PORT=config.get('CLUSTER','DB_PORT')
-CLUSTER_ENDPOINT=config.get('CLUSTER','ENDPOINT')
+CLUSTER_TYPE=config.get('CLUSTER','TYPE')
+CLUSTER_NODE_TYPE=config.get('CLUSTER','NODE_TYPE')
+CLUSTER_IDENTIFIER=config.get('CLUSTER','IDENTIFIER')
+CLUSTER_NUM_NODES=config.get('CLUSTER','NUM_NODES')
+CLUSTER_ENDPOINT='ENDPOINT'
 
 S3_LOG_DATA=config.get('S3', 'LOG_DATA')
 S3_LOG_JSONPATH=config.get('S3', 'LOG_JSONPATH')
@@ -19,10 +21,5 @@ S3_SONG_DATA=config.get('S3', 'SONG_DATA')
 
 IAM_ROLE_ARN=config.get('IAM_ROLE','ARN')
 
-
-def connect_redshift():
-    conn_string="postgresql://{}:{}@{}:{}/{}".format(
-        CLUSTER_DB_USER, CLUSTER_DB_PASSWORD, CLUSTER_ENDPOINT, CLUSTER_PORT,CLUSTER_DB
-    )
-    print(conn_string)
-    # %sql $conn_string
+def set_cluster_endpoint(endpoint):
+    CLUSTER_ENDPOINT=endpoint

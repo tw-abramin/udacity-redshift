ae5294b62a9c69d6619a88ab141e73aa993dee20
Alexander Ramin <alex.ramin@thoughtworks.com>
2021-09-01 09:40:31 +0200

    Add some output for status

--- etl.py
+++ etl.py
@@ -4,16 +4,21 @@ from sql_queries import copy_table_queries, insert_table_queries
 
 
 def load_staging_tables(cur, conn):
+    print("Loading Staging Tables...")
     for query in copy_table_queries:
+        print(query)
         cur.execute(query)
         conn.commit()
+    print("Done")
 
 
 def insert_tables(cur, conn):
+    print("Loading data into tables...")
     for query in insert_table_queries:
+        print(query)
         cur.execute(query)
         conn.commit()
-
+    print("Done")
 
 def main():
     config = configparser.ConfigParser()

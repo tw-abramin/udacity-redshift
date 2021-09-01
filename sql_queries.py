1207e13c53e62d74172d1edde6a9a5b08bec3a2e
Alexander Ramin <alex.ramin@thoughtworks.com>
2021-09-01 13:56:23 +0200

    ETL Working, no constraints or dist keys

--- sql_queries.py
+++ sql_queries.py
@@ -24,18 +24,18 @@ artist_name VARCHAR,
 auth VARCHAR,
 first_name VARCHAR,
 last_name VARCHAR,
-gender CHAR,
-item_in_session INTEGER,
+gender CHAR(3),
+item_in_session VARCHAR,
 length FLOAT,
-level CHAR,
+level VARCHAR,
 location VARCHAR,
-method CHAR,
-page CHAR,
+method CHAR(5),
+page VARCHAR,
 registration FLOAT,
 session_id INTEGER,
 song_title VARCHAR,
 status SMALLINT,
-start_time BIGINT,
+time_stamp BIGINT,
 user_agent VARCHAR,
 user_id VARCHAR)
 """)
@@ -44,7 +44,7 @@ staging_songs_table_create = ("""
 CREATE TABLE IF NOT EXISTS staging_songs (
 artist_id VARCHAR,
 artist_latitude FLOAT,
-artist_location FLOAT,
+artist_location VARCHAR,
 artist_longitude FLOAT,
 artist_name VARCHAR,
 duration FLOAT,
@@ -59,7 +59,7 @@ CREATE TABLE IF NOT EXISTS users (
 user_id VARCHAR NOT NULL PRIMARY KEY SORTKEY,
 first_name VARCHAR,
 last_name VARCHAR,
-gender CHAR,
+gender CHAR(3),
 level VARCHAR)
 """)
 
@@ -107,23 +107,23 @@ weekday INTEGER)
 
 # STAGING TABLES
 ## TODO: remove hardcoded ARN
-staging_events_copy = ("""
-copy staging_events from 's3://udacity-dend/song_data' 
+staging_events_copy = (f"""
+copy staging_events from 's3://udacity-dend/log_data' 
 credentials 'aws_iam_role=arn:aws:iam::590606803980:role/DWH_IAM_REDSHIFT_TEST_ROLE'
-format as json 'auto' region 'us-west-2';
+format as json { aws_helper.S3_LOG_JSONPATH } compupdate off region 'us-west-2';
 """).format()
 
 staging_songs_copy = ("""
-copy staging_songs from 's3://udacity-dend/log_data' 
+copy staging_songs from 's3://udacity-dend/song_data' 
 credentials 'aws_iam_role=arn:aws:iam::590606803980:role/DWH_IAM_REDSHIFT_TEST_ROLE'
-format as json 'auto' region 'us-west-2';
+format as json 'auto' compupdate off region 'us-west-2';
 """).format()
 
 # FINAL TABLES
 
 songplay_table_insert = ("""
 INSERT INTO songplays (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
-SELECT DISTINCT se.start_time as start_time,
+SELECT DISTINCT se.time_stamp as start_time,
                 se.user_id as user_id,
                 se.level as level,
                 ss.song_id as song_id,
@@ -142,7 +142,8 @@ SELECT DISTINCT se.user_id as user_id,
                 se.last_name as last_name,
                 se.gender as gender,
                 se.level as level
-FROM staging_events se;
+FROM staging_events se
+WHERE se.page='NextSong';
 """)
 
 song_table_insert = ("""
@@ -157,7 +158,7 @@ JOIN staging_songs ss ON se.song_title = ss.title;
 """)
 
 artist_table_insert = ("""
-INSERT INTO songs (artist_id, name, location, latitude, longitude)
+INSERT INTO artists (artist_id, name, location, latitude, longitude)
 SELECT DISTINCT ss.artist_id as artist_id,
                 se.artist_name as name,
                 se.location as location,
@@ -168,14 +169,14 @@ JOIN staging_songs ss ON se.artist_name = ss.artist_name;
 """)
 
 time_table_insert = ("""INSERT INTO TIME (start_time, hour, day, week, month, year, weekday)
-SELECT DISTINCT se.start_time,
-                EXTRACT (HOUR FROM se.start_time), 
-                EXTRACT (DAY FROM se.start_time),
-                EXTRACT (WEEK FROM se.start_time), 
-                EXTRACT (MONTH FROM se.start_time),
-                EXTRACT (YEAR FROM se.start_time), 
-                EXTRACT (WEEKDAY FROM se.start_time) 
-FROM (SELECT TIMESTAMP 'epoch' + start_time/1000 *INTERVAL '1 second' as start_time FROM staging_events) se;
+SELECT DISTINCT start_time,
+                EXTRACT (HOUR FROM start_time), 
+                EXTRACT (DAY FROM start_time),
+                EXTRACT (WEEK FROM start_time), 
+                EXTRACT (MONTH FROM start_time),
+                EXTRACT (YEAR FROM start_time), 
+                EXTRACT (WEEKDAY FROM start_time) 
+FROM (SELECT DISTINCT time_stamp, 'epoch'::date + time_stamp/1000 *INTERVAL '1 second' as start_time FROM staging_events);
 """)
 
 # QUERY LISTS
